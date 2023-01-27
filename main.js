d8f1fa53ab7f8e42d1f4fba2bd08edda8a2454c2
Princekashyap <vasukashyap08@gmail.com>
2023-01-27 18:22:34 +0530

    this project is complete i code all the section

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(".carousel").owlCarousel({
     margin: 30,
     loop: true,
     dots: false,
-    autoplay: false,
+    autoplay: true,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
     responsive:{
@@ -20,8 +20,43 @@ $(".carousel").owlCarousel({
      },
     }
  });
- 
+
+//  sticky-header 
  window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 20);
  });
+
+// hamburger 
+ const hamburger = document.querySelector(".hamburger");
+ const navMenu = document.querySelector(".nav-menu");
+
+ hamburger.addEventListener("click",mobileMenu);
+
+ function mobileMenu(){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+ };
+ const navLink = document.querySelectorAll(".nav-link");
+
+ navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+ function closeMenu() {
+     hamburger.classList.remove("active");
+     navMenu.classList.remove("active");
+ };
+
+//  toptoscroll 
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+        mybutton.style.display = "block";
+    }else{
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
